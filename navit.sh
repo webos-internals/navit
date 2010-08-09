aa18c386f0b34df5c6d1c121e5ef817bee0bdcd4
Malte Schröder <maltesch@gmx.de>
2010-08-09 17:04:23 +0200

    migration is done in postinst, remove from navit.sh and polish postinst a bit

--- navit.sh
+++ navit.sh
@@ -18,6 +18,7 @@ export SPEECHD_SOCKET=/tmp/speechd-sock
 export NAVIT_LOGFILE=$NAVIT_USER_DATADIR/navit.log
 
 test -d $NAVIT_USER_DATADIR || mkdir -p $NAVIT_USER_DATADIR/maps
+test -e $NAVIT_USER_DATADIR/navit.xml || cp $APP_DIR/dist_files/navit*.xml $NAVIT_USER_DATADIR/                                                                 
 
 #delete log if it is more the 300kb
 touch $NAVIT_LOGFILE
@@ -29,16 +30,5 @@ fi
 echo "------------------------- Start Navit ----------------------------------" >> $NAVIT_LOGFILE
 date >> $NAVIT_LOGFILE
 
-# Migrate data to new directories
-if test -d /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0 
-then
-	mv /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0/* $NAVIT_USER_DATADIR
-	mv /media/internal/MapsNavit/* $NAVIT_USER_DATADIR/maps
-	rmdir /media/internal/MapsNavit
-	sed -i -re "s/\/media\/internal\/MapsNavit/\$NAVIT_USER_DATADIR\/maps/"  $NAVIT_USER_DATADIR/navit.xml
-	rmdir /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0
-fi
-
-test -e $NAVIT_USER_DATADIR/navit.xml || cp $APP_DIR/dist_files/navit*.xml $NAVIT_USER_DATADIR/                                                                 
 pgrep "^navit$" || exec $APP_DIR/bin/navit -c $NAVIT_USER_DATADIR/navit.xml
 
